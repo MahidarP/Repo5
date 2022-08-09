b12d25daabf14c85fc61951fdd33cc68051a3b8e
MahidarP <110811646+MahidarP@users.noreply.github.com>
2022-08-09 10:04:17 +0530

    Add files via upload

---
+++ Class12.cs
@@ -0,0 +1,32 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+     interface IAnimal
+     {
+        void animalsound();
+     }
+
+    class pig : IAnimal
+    {
+            public void animalsound()
+            {
+              Console.WriteLine("The pig sound is wee wee");
+            }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pig mypig = new pig();
+            mypig.animalsound();
+        }
+    }
+
+} */
